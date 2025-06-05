a95f3a0e5d3ec313e67f02d35362afc79f268e6b
the42 <fridolin_1@yahoo.com>
2025-06-05 17:30:43 +0200

    #2 Snippets for the recipes

---
+++ src/main/kotlin/models/snippets/RecipeSnippets.kt
@@ -0,0 +1,55 @@
+package de.fridolin1.models.snippets
+
+import de.fridolin1.models.cooking.Recipe
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RecipeHead(
+    val id: Int,
+    val name: String,
+    val minimumAge: Int,
+    val timeExpenditure: String
+)
+
+fun Recipe.toRecipeHead(): RecipeHead = RecipeHead(
+    this.id.value,
+    this.name,
+    this.minimumAge,
+    this.timeExpenditure
+)
+
+@Serializable
+data class RecipeSearch(
+    val id: Int,
+    val name: String,
+    val minimumAge: Int,
+    val timeExpenditure: String,
+    val ingredientIDs: List<Int>
+)
+
+fun Recipe.toRecipeSearchObject(): RecipeSearch = RecipeSearch(
+    this.id.value,
+    this.name,
+    this.minimumAge,
+    this.timeExpenditure,
+    this.recipesIngredients.map { it.ingredient.id.value }
+)
+
+@Serializable
+data class RawRecipe(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val minimumAge: Int,
+    val timeExpenditure: String,
+    val ingredientIDs: List<Int>
+)
+
+fun Recipe.toRawRecipe(): RawRecipe = RawRecipe(
+    this.id.value,
+    this.name,
+    this.description,
+    this.minimumAge,
+    this.timeExpenditure,
+    this.recipesIngredients.map { it.ingredient.id.value }
+)
