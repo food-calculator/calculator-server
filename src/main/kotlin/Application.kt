c618e6ddb5420304a87dc622a48f65714b7878cd
the42 <fridolin_1@yahoo.com>
2025-06-05 22:01:21 +0200

    installed ContentNegotiation plugin

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -3,7 +3,10 @@ package de.fridolin1
 import de.fridolin1.modules.enableCors
 import de.fridolin1.modules.configureMonitoring
 import de.fridolin1.modules.configureRouting
+import de.fridolin1.utils.json
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -13,4 +16,7 @@ fun Application.module() {
     enableCors()
     configureMonitoring()
     configureRouting()
+    install(ContentNegotiation) {
+        json(json)
+    }
 }
---
+++ src/main/kotlin/utils/Json.kt
@@ -0,0 +1,7 @@
+package de.fridolin1.utils
+
+import kotlinx.serialization.json.Json
+
+val json = Json {
+    ignoreUnknownKeys = true
+}
