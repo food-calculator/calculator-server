9329bc807d61b9f433f75eb3f772e20bda07fcbf
the42 <fridolin_1@yahoo.com>
2025-06-10 14:16:30 +0200

    update cors to allow all crud operations

--- src/main/kotlin/modules/HTTP.kt
+++ src/main/kotlin/modules/HTTP.kt
@@ -6,12 +6,10 @@ import io.ktor.server.plugins.cors.routing.*
 
 fun Application.enableCors() {
     install(CORS) {
-        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Get)
         allowMethod(HttpMethod.Put)
         allowMethod(HttpMethod.Delete)
-        allowMethod(HttpMethod.Patch)
-        allowHeader(HttpHeaders.Authorization)
-        allowHeader("MyCustomHeader")
         anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
     }
 //    routing {
