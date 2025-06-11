6114fe656adcfb957ee6680aafce559d4ec2691b
fridolin <fridolin_1@yahoo.com>
2025-06-11 16:06:29 +0200

    allow all cors request

--- src/main/kotlin/modules/HTTP.kt
+++ src/main/kotlin/modules/HTTP.kt
@@ -1,20 +1,14 @@
 package de.fridolin1.modules
 
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
 
 fun Application.enableCors() {
-//    install(CORS) {
-//        allowMethod(HttpMethod.Post)
-//        allowMethod(HttpMethod.Get)
-//        allowMethod(HttpMethod.Put)
-//        allowMethod(HttpMethod.Delete)
-//        allowMethod(HttpMethod.Options)
-//        allowHeader(HttpHeaders.Authorization)
-//        allowCredentials = true
-//        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
-//    }
+    install(CORS) {
+        allowCredentials = true
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+        anyMethod()
+    }
 //    routing {
 //        openAPI(path = "openapi") @TODO configure the openAPI
 //    }
